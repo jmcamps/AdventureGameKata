89e3fd93e2342a7690ace8d8afb4effd5fdf2182
Josep Maria Camps i Riba <jmcamps@gmail.com>
2014-08-28 11:35:42 +0200

    Initial import

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := """AdventureGameKata"""
+
+version := "1.0"
+
+scalaVersion := "2.11.1"
+
+libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.0" % "test"
+
+libraryDependencies += "junit" % "junit" % "4.10" % "test"
---
+++ src/test/scala/cat/xampi/kata/adventure/AdventureGameSpec.scala
@@ -0,0 +1,17 @@
+package cat.xampi.kata.adventure
+
+import org.scalatest._
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+// add JUnit runner annotation to allow Infinitest work with Eclipse
+@RunWith(classOf[JUnitRunner])
+class AdventureGameSpec extends FlatSpec with Matchers {
+
+  "Treasure" should "be buyed in happy path" in { 
+     
+    
+  }
+
+  
+}
